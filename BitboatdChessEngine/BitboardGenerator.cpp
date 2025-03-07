a681d13a5132ffe31ee24f965d52689d984a641e
Alex7342 <alexborgovan@yahoo.com>
2025-03-07 16:45:29 +0200

    Added a BitboardGenerator class. It can shift bitboards in every direction and generate moveset bitboards for pawns and knights.

---
+++ BitboatdChessEngine/BitboardGenerator.cpp
@@ -0,0 +1,44 @@
+#include "BitboardGenerator.h"
+
+uint64_t BitboardGenerator::generatePawnPush(const uint64_t pawnBitboard, const int color)
+{
+	if (color == 0)
+	{
+		return north(pawnBitboard);
+	}
+	else if (color == 1)
+	{
+		return south(pawnBitboard);
+	}
+
+	return 0;
+}
+
+uint64_t BitboardGenerator::generatePawnAttack(const uint64_t pawnBitboard, const int color)
+{
+	if (color == 0)
+	{
+		return northWest(pawnBitboard) | northEast(pawnBitboard);
+	}
+	else if (color == 1)
+	{
+		return southWest(pawnBitboard) | southEast(pawnBitboard);
+	}
+
+	return 0;
+}
+
+uint64_t BitboardGenerator::generateKnightMoveset(const uint64_t knightBitboard)
+{
+	uint64_t movePattern =
+		north(west(west(knightBitboard))) |   // 1 square up, 2 squares left
+		west(north(north(knightBitboard))) |  // 2 squares up, 1 square left
+		east(north(north(knightBitboard))) |  // 2 squares up, 1 square right
+		north(east(east(knightBitboard))) |   // 1 square up, 2 squares right
+		south(east(east(knightBitboard))) |   // 1 square down, 2 squares right
+		east(south(south(knightBitboard))) |  // 2 squares down, 1 square right
+		west(south(south(knightBitboard))) |  // 2 squares down, 1 square left
+		south(west(west(knightBitboard)));    // 1 square down, 2 squares left
+
+	return movePattern;
+}
