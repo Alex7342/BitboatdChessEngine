22f078b1df0c84df86dc8fc0dd1037551332ab42
Alex7342 <alexborgovan@yahoo.com>
2025-03-21 20:12:03 +0200

    The toString method of the Move class now handles promotion

--- BitboatdChessEngine/Move.cpp
+++ BitboatdChessEngine/Move.cpp
@@ -4,7 +4,29 @@ Move::Move() : moveData(0) {}
 
 std::string Move::toString() const
 {
-	return std::to_string(this->from()) + " -> " + std::to_string(this->to());
+	std::string moveString = std::to_string(this->from()) + " -> " + std::to_string(this->to());
+
+	if (moveType() == MoveType::PROMOTION)
+	{
+		PromotionPiece piece = promotionPiece();
+		switch (piece)
+		{
+		case PromotionPiece::QUEEN:
+			moveString += " Q";
+			break;
+		case PromotionPiece::KNIGHT:
+			moveString += " N";
+			break;
+		case PromotionPiece::ROOK:
+			moveString += " R";
+			break;
+		case PromotionPiece::BISHOP:
+			moveString += " B";
+			break;
+		}
+	}
+
+	return moveString;
 }
 
 MoveList::MoveList() : numberOfMoves(0) {}
